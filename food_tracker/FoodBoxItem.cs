4b5c717e3eda0f2d3e6cf48616caa6513ff12805
Luke Cahill <lmc@outlook.com>
2017-08-12 15:35:53 +0100

    Add custom listbox item class

---
+++ food_tracker/FoodBoxItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace food_tracker {
+	public class FoodBoxItem : ListBoxItem {
+
+		public int calories { get; set; }
+		public int fats { get; set; }
+		public int saturatedFat { get; set; }
+		public int carbohydrates { get; set; }
+		public int sugar { get; set; }
+		public int protein { get; set; }
+		public int salt { get; set; }
+		public string name { get; set; }
+
+		public FoodBoxItem() : base() { }
+
+		public FoodBoxItem(int cals, int fats, int satFat, int carbs, int sugars, int protein, int salt, string name) {
+			this.name = name;
+			this.calories = cals;
+			this.fats = fats;
+			this.salt = salt;
+			this.saturatedFat = satFat;
+			this.carbohydrates = carbs;
+			this.sugar = sugars;
+			this.protein = protein;
+		}
+
+		public override string ToString() {
+			return name;
+		}
+	}
+}
