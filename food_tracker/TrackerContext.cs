c97758ee9a8866c097799165b7fe700e0559210e
Luke Cahill <lmc@outlook.com>
2017-08-12 17:24:56 +0100

    Add entity framework and add DB Context class

---
+++ food_tracker/TrackerContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.Entity;
+
+namespace food_tracker {
+    public class TrackerContext : DbContext {
+        // need to think what DB sets are needed
+        public DbSet<WholeDay> Days { get; set; }
+    }
+}
---
+++ food_tracker/WholeDay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace food_tracker {
+    public class WholeDay {
+
+        public DateTime dateTime { get; set; }
+
+        public virtual List<NutritionItem> foodsDuringDay { get; set; }
+
+        public WholeDay(DateTime date) {
+            this.dateTime = date;
+        }
+    }
+}
