74d6e2e115d120f9f0972e0d503b05984b05f11a
Luke Cahill <lmc@outlook.com>
2017-08-12 17:25:31 +0100

    Add items for database persistence

---
+++ food_tracker/NutritionItem.cs
@@ -0,0 +1,25 @@
+﻿namespace food_tracker {
+    public class NutritionItem {
+        public string name { get; set; }
+        public double calories { get; set; }
+        public double carbohydrates { get; set; }
+        public double sugars { get; set; }
+        public double fats { get; set; }
+        public double saturatedFats { get; set; }
+        public double protein { get; set; }
+        public double salt { get; set; }
+        public double fibre { get; set; }
+
+        public NutritionItem(string name, double calories, double carbohydrates, double sugars, double fats, double satFat, double protein, double salt, double fibre) {
+            this.name = name;
+            this.calories = calories;
+            this.carbohydrates = carbohydrates;
+            this.sugars = sugars;
+            this.fats = fats;
+            this.saturatedFats = satFat;
+            this.protein = protein;
+            this.salt = salt;
+            this.fibre = fibre;
+        }
+    }
+}
