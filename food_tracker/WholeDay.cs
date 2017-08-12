0def4524081d5332aed32528f9019279607d544f
Luke Cahill <lmc@outlook.com>
2017-08-12 18:39:03 +0100

    Now using data annotations Add more DBSets to context

--- food_tracker/WholeDay.cs
+++ food_tracker/WholeDay.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
 namespace food_tracker {
     public class WholeDay {
 
+        [Key]
+        public int WholeDayId { get; set; }
         public DateTime dateTime { get; set; }
-
         public virtual List<NutritionItem> foodsDuringDay { get; set; }
 
         public WholeDay(DateTime date) {
