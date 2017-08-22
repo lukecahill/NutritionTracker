f237b9cf85128d167c0d0bbe73cfa94ee7704cbb
Luke Cahill <lmc@outlook.com>
2017-08-22 09:42:18 +0100

    Add helper class and begin moving some items into this

---
+++ food_tracker/Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace food_tracker {
+    public class Helper {
+
+        public double[] calculateTotals(ListBox boxItem) {
+
+            // calories, fats, sats, carbs, sugars, proteins, salts, fibre
+            double[] values = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            foreach (FoodBoxItem item in boxItem.Items) {
+                values[0] += item.calories;
+                values[1] += item.fats;
+                values[2] += item.saturatedFat;
+                values[3] += item.carbohydrates;
+                values[4] += item.sugar;
+                values[5] += item.protein;
+                values[6] += item.salt;
+                values[7] += item.fibre;
+            }
+
+            return values;
+        }
+
+        public double parseTextBoxForDouble(TextBox text) {
+            var parsed = 0.0D;
+            if (double.TryParse(text.Text, out parsed)) {
+                // suceeded to parse
+                Debug.WriteLine($"Parsed number: {parsed}");
+            } else {
+                Debug.WriteLine($"Could not parse {text.Text} - ensure that this is a valid number.");
+                MessageBox.Show("Error when attempting to parse integer values");
+            }
+
+            return parsed;
+        }
+    }
+}
