601a0a4b2822cd877129b835af594a49ada2ba0d
Luke Cahill <lmc@outlook.com>
2017-08-23 10:44:45 +0100

    Add interface for common properties

---
+++ food_tracker/IListItems.cs
@@ -0,0 +1,15 @@
+﻿namespace food_tracker {
+    public interface IListItems {
+        double calories { get; set; }
+        double amount { get; set; }
+        double fats { get; set; }
+        double saturatedFat { get; set; }
+        double carbohydrates { get; set; }
+        double sugar { get; set; }
+        double protein { get; set; }
+        double salt { get; set; }
+        double fibre { get; set; }
+        string name { get; set; }
+        int nutritionId { get; set; }
+    }
+}
