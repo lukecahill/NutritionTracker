4492f456476b83e575064bf71bbd3cf85655af2c
Luke Cahill <lmc@outlook.com>
2017-08-29 16:01:59 +0100

    Explicitly specifiy the database name.
    Add logging and disable lazy-loading

--- food_tracker/TrackerContext.cs
+++ food_tracker/TrackerContext.cs
@@ -2,6 +2,13 @@
 
 namespace food_tracker {
     public class TrackerContext : DbContext {
+
+        public TrackerContext() : base("name=NutritionTrackerContext") {
+            Configuration.LazyLoadingEnabled = false;
+
+            this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+        }
+
         public DbSet<WholeDay> Days { get; set; }
         public DbSet<NutritionItem> Nutrition { get; set; }
     }
