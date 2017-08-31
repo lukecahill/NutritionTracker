d816f1d80f88e9b529c3476d5d11995dba4a3bb4
Luke Cahill <lmc@outlook.com>
2017-08-31 14:25:43 +0100

    Interface for database context to implement

---
+++ food_tracker/DAL/IMyEntitiesContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace food_tracker {
+    public interface IMyEntitiesContext {
+        IDbSet<NutritionItem> Nutrition { get; set; }
+        IDbSet<WholeDay> Days { get; set; }
+
+        int SaveChanges();
+    }
+}
