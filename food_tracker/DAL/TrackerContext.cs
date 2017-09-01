f840d74afefee5768c99fb3904e67ad7a294a91e
Luke Cahill <lmc@outlook.com>
2017-09-01 09:48:07 +0100

    Remove database context interface

--- food_tracker/DAL/TrackerContext.cs
+++ food_tracker/DAL/TrackerContext.cs
@@ -1,8 +1,7 @@
 ﻿using System.Data.Entity;
-using food_tracker.Interfaces;
 
 namespace food_tracker.DAL {
-    public class TrackerContext : DbContext, IMyEntitiesContext {
+    public class TrackerContext : DbContext {
 
         public TrackerContext() : base() {
             Configuration.LazyLoadingEnabled = false;
@@ -12,9 +11,5 @@ namespace food_tracker.DAL {
 
         public IDbSet<WholeDay> Days { get; set; }
         public IDbSet<NutritionItem> Nutrition { get; set; }
-        
-        public override int SaveChanges() {
-            return base.SaveChanges();
-        }
     }
 }
