784024ea7a1909acd5bba6563bedd9029cefc46f
Luke Cahill <lmc@outlook.com>
2017-09-01 13:29:20 +0100

    Change List data type to IEnumerable

--- food_tracker/DAL/WholeDay.cs
+++ food_tracker/DAL/WholeDay.cs
@@ -11,7 +11,7 @@ namespace food_tracker.DAL {
         public string WholeDayId { get; set; }
         public DateTime dateTime { get; set; }
         public double dailyTotal { get; set; }
-        public virtual List<NutritionItem> foodsDuringDay { get; set; }
+        public virtual IEnumerable<NutritionItem> foodsDuringDay { get; set; }
 
         [Obsolete("Only needed for serialization and materialization", true)]
         public WholeDay() { }
